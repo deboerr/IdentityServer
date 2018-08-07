cf5f47f02bff7a2f3fda64a7c4751ab944721022
deboerr <deboerr@luxbet.com>
2018-08-07 22:06:47 +1000

    first commit

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,32 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client {
+                    ClientId = "angular_spa",
+                    ClientName = "Angular 4 Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowedScopes = new List<string> { "openid", "profile", "api1" },
+                    RedirectUris = new List<string> { "http://localhost:4200/auth-callback" },
+                    PostLogoutRedirectUris = new List<string> { "http://localhost:4200/" },
+                    AllowedCorsOrigins = new List<string> { "http://localhost:4200" },
+                    AllowAccessTokensViaBrowser = true
+                }
+            };
+        }
+
+    }
+}
